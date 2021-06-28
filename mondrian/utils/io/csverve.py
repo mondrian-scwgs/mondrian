0b443b611aa5f1620884c8c14c285bc72a4e1fb4
Diljot Grewal <diljot.grewal@gmail.com>
2021-06-28 15:09:37 -0700

    minor

--- mondrian/utils/io/csverve.py
+++ mondrian/utils/io/csverve.py
@@ -18,7 +18,7 @@ def rewrite_csv(infile, outfile, dtypes):
     else:
         raise Exception()
 
-    csverve.rewrite_csv_file(infile, outfile, dtypes=dtypes)
+    csverve.rewrite_csv_file(infile, outfile, dtypes=dtypes, write_header=False)
 
 
 def parse_args():
