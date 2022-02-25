144eec6317f5855d43c031441ee8bbddc5fd9871
Diljot Grewal <diljot.grewal@gmail.com>
2022-02-25 09:49:40 -0800

    adding buildspec

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,33 @@
-TYPE=$1
-VERSION=$2
+#!/bin/bash
 
+TYPE=$1
+PUSH=$2
+CACHE=$3
+USERNAME=$4
+PASSWORD=$5
+
+
+VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 
 cd $TYPE
 
-docker build --build-arg VERSION=$VERSION -t quay.io/mondrianscwgs/$TYPE:$VERSION . --no-cache
-docker push quay.io/mondrianscwgs/$TYPE:$VERSION
+
+CMD="docker build --build-arg VERSION=$VERSION -t quay.io/mondrianscwgs/$TYPE:$VERSION ."
+
+
+if [ $CACHE == "Y" ]
+then
+    $CMD
+else
+    $CMD --no-cache
+fi
+
+
+if [ $PUSH == "Y" ]
+then
+    docker login quay.io -u $USERNAME --password $PASSWORD
+    docker push quay.io/mondrianscwgs/$TYPE:$VERSION
+fi
+
 
 cd ../
