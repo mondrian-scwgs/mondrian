6ad2ccc97779971fa44efbd5c3348da9af7555fa
Diljot Grewal <diljot.grewal@gmail.com>
2022-04-28 12:42:55 -0700

    minor

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -5,9 +5,6 @@ wget -nv https://mondriantestdata.s3.amazonaws.com/result_reference.tar.gz
 tar -xvf result_reference.tar.gz
 
 
-#ls $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/
-#ls result_reference/
-
 #docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_alignment \
 #    --metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_metrics.csv.gz --metrics_ref result_reference/alignment_metrics.csv.gz \
 #    --gc_metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_gc_metrics.csv.gz --gc_metrics_ref result_reference/alignment_gc_metrics.csv.gz
@@ -24,13 +21,13 @@ tar -xvf result_reference.tar.gz
 #    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
 #    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
 
-docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_variant_calling \
-    --museq $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_museq.vcf.gz --museq_ref result_reference/museq.vcf.gz \
-    --mutect $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_mutect.vcf.gz --mutect_ref result_reference/mutect.vcf.gz \
-    --strelka_indel $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_indel.vcf.gz --strelka_indel_ref result_reference/strelka_indel.vcf.gz \
-    --strelka_snv $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_snv.vcf.gz --strelka_snv_ref result_reference/strelka_snv.vcf.gz \
+#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_variant_calling \
+#    --museq $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_museq.vcf.gz --museq_ref result_reference/museq.vcf.gz \
+#    --mutect $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_mutect.vcf.gz --mutect_ref result_reference/mutect.vcf.gz \
+#    --strelka_indel $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_indel.vcf.gz --strelka_indel_ref result_reference/strelka_indel.vcf.gz \
+#    --strelka_snv $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_snv.vcf.gz --strelka_snv_ref result_reference/strelka_snv.vcf.gz \
 
 
 docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_snv_genotyping \
-    --genotyper $CODEBUILD_SRC_DIR/tests/snv_genotyping/outputs/results/genotyper.csv.gz --museq_ref result_reference/genotyper.csv.gz \
-    --vartrix $CODEBUILD_SRC_DIR/tests/snv_genotyping/outputs/results/vartrix.csv.gz --mutect_ref result_reference/vartrix.csv.gz \
+    --genotyper $CODEBUILD_SRC_DIR/tests/snv_genotyping/outputs/results/genotyper.csv.gz --genotyper_ref result_reference/genotyper.csv.gz \
+    --vartrix $CODEBUILD_SRC_DIR/tests/snv_genotyping/outputs/results/vartrix.csv.gz --vartrix_ref result_reference/vartrix.csv.gz \
