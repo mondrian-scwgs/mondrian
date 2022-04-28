86d2f054861e6464f76043d39bffbfed647a3679
Diljot Grewal <diljot.grewal@gmail.com>
2022-04-28 08:54:50 -0700

    minor

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -5,9 +5,8 @@ wget -nv https://mondriantestdata.s3.amazonaws.com/result_reference.tar.gz
 tar -xvf result_reference.tar.gz
 
 
-ls $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/
-
-ls result_reference/
+#ls $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/
+#ls result_reference/
 
 #docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_alignment \
 #    --metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_metrics.csv.gz --metrics_ref result_reference/alignment_metrics.csv.gz \
