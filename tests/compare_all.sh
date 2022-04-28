9f91e5676132998103baafa370f5bb919867440f
Diljot Grewal <diljot.grewal@gmail.com>
2022-04-28 10:07:11 -0700

    minor

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -19,7 +19,7 @@ tar -xvf result_reference.tar.gz
 
 
 docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
-    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint/outputs/results/T2-T-A_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
-    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint/outputs/results/T2-T-A_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
-    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint/outputs/results/T2-T-A_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
-    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint/outputs/results/T2-T-A.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
+    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
+    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
+    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
+    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
