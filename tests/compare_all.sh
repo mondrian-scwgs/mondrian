517558d6018a3fb3870f9d6bf8ff1328a8bc1299
Diljot Grewal <diljot.grewal@gmail.com>
2022-08-25 11:29:59 -0700

    fix

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -7,15 +7,15 @@ wget -nv https://mondriantestdata.s3.amazonaws.com/result_reference.tar.gz
 tar -xvf result_reference.tar.gz
 
 
-docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_alignment \
-    --metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_workflow_alignment_metrics.csv.gz --metrics_ref result_reference/alignment_metrics.csv.gz \
-    --gc_metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_workflow_alignment_gc_metrics.csv.gz --gc_metrics_ref result_reference/alignment_gc_metrics.csv.gz
+#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_alignment \
+#    --metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_workflow_alignment_metrics.csv.gz --metrics_ref result_reference/alignment_metrics.csv.gz \
+#    --gc_metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_workflow_alignment_gc_metrics.csv.gz --gc_metrics_ref result_reference/alignment_gc_metrics.csv.gz
 
 
-#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_hmmcopy \
-#    --reads $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_reads.csv.gz --reads_ref result_reference/hmmcopy_reads.csv.gz \
-#    --metrics $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_metrics.csv.gz --metrics_ref result_reference/hmmcopy_metrics.csv.gz
-#
+docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/hmmcopy:${TAG}beta mondrian_build_utils compare_hmmcopy \
+    --reads $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_reads.csv.gz --reads_ref result_reference/hmmcopy_reads.csv.gz \
+    --metrics $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_metrics.csv.gz --metrics_ref result_reference/hmmcopy_metrics.csv.gz
+
 #
 #docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
 #    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
