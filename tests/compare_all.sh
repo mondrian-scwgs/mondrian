019bdd1facfcb070445772b6a2cc07753c975b28
Diljot Grewal <diljot.grewal@gmail.com>
2022-08-25 13:20:21 -0700

    fix

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -17,17 +17,17 @@ tar -xvf result_reference.tar.gz
 #    --metrics $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_hmmcopy_metrics.csv.gz --metrics_ref result_reference/hmmcopy_metrics.csv.gz
 
 
-docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
-    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
-    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
-    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
-    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
-#
-#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_variant_calling \
-#    --museq $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_museq.vcf.gz --museq_ref result_reference/museq.vcf.gz \
-#    --mutect $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_mutect.vcf.gz --mutect_ref result_reference/mutect.vcf.gz \
-#    --strelka_indel $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_indel.vcf.gz --strelka_indel_ref result_reference/strelka_indel.vcf.gz \
-#    --strelka_snv $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_strelka_snv.vcf.gz --strelka_snv_ref result_reference/strelka_snv.vcf.gz \
+#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
+#    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
+#    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
+#    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
+#    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
+
+docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_variant_calling \
+    --museq $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/variant_calling_museq.vcf.gz --museq_ref result_reference/museq.vcf.gz \
+    --mutect $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/variant_calling_mutect.vcf.gz --mutect_ref result_reference/mutect.vcf.gz \
+    --strelka_indel $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/variant_calling_strelka_indel.vcf.gz --strelka_indel_ref result_reference/strelka_indel.vcf.gz \
+    --strelka_snv $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/variant_calling_strelka_snv.vcf.gz --strelka_snv_ref result_reference/strelka_snv.vcf.gz \
 #
 #
 #docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_snv_genotyping \
