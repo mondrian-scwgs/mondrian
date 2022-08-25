98e11440e6b0075e5d720afbf7a68aedd2472fc1
Diljot Grewal <diljot.grewal@gmail.com>
2022-08-25 12:50:23 -0700

    fix

--- tests/compare_all.sh
+++ tests/compare_all.sh
@@ -12,16 +12,16 @@ tar -xvf result_reference.tar.gz
 #    --gc_metrics $CODEBUILD_SRC_DIR/tests/alignment/outputs/results/alignment_workflow_alignment_gc_metrics.csv.gz --gc_metrics_ref result_reference/alignment_gc_metrics.csv.gz
 
 
-docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/hmmcopy:${TAG}beta mondrian_build_utils compare_hmmcopy \
-    --reads $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_hmmcopy_reads.csv.gz --reads_ref result_reference/hmmcopy_reads.csv.gz \
-    --metrics $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_hmmcopy_metrics.csv.gz --metrics_ref result_reference/hmmcopy_metrics.csv.gz
+#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/hmmcopy:${TAG}beta mondrian_build_utils compare_hmmcopy \
+#    --reads $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_hmmcopy_reads.csv.gz --reads_ref result_reference/hmmcopy_reads.csv.gz \
+#    --metrics $CODEBUILD_SRC_DIR/tests/hmmcopy/outputs/results/hmmcopy_hmmcopy_metrics.csv.gz --metrics_ref result_reference/hmmcopy_metrics.csv.gz
 
-#
-#docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
-#    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
-#    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
-#    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
-#    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/T2-T-A.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
+
+docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_breakpoint_calling \
+    --destruct $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_breakpoint_table.csv --destruct_ref result_reference/destruct.csv \
+    --gridss $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_gridss.vcf.gz --gridss_ref result_reference/gridss.vcf.gz \
+    --lumpy $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint_lumpy.vcf --lumpy_ref result_reference/lumpy.vcf.gz \
+    --svaba $CODEBUILD_SRC_DIR/tests/breakpoint_calling/outputs/results/breakpoint.svaba.somatic.sv.vcf.gz --svaba_ref result_reference/svaba.vcf.gz \
 #
 #docker run -w $PWD -v $PWD:$PWD -v $CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR quay.io/mondrianscwgs/alignment:${TAG}beta mondrian_build_utils compare_variant_calling \
 #    --museq $CODEBUILD_SRC_DIR/tests/variant_calling/outputs/results/SA123T_museq.vcf.gz --museq_ref result_reference/museq.vcf.gz \
